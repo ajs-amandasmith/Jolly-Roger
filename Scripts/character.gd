de992605cdc34f8c036ce1e401b9e8adf1411203
Amanda Smith <ajs.amandasmith@gmail.com>
2024-06-24 22:29:43 -0500

    Animate Character

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
-
+@export_category("Locomotion")
 @export var _speed = 8
 @export var _acceleration : float = 16
 @export var _deceleration : float = 32
-const JUMP_VELOCITY = -400.0
+
+@export_category("Jump")
+@export var _jump_height : float = 2.5
+@export var _air_control : float = 0.5
+var _jump_velocity : float
+
+@onready var _sprite : Sprite2D = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,29 +21,45 @@ func _ready():
 	_speed *= Global.ppt
 	_acceleration *= Global.ppt
 	_deceleration *= Global.ppt
+	_jump_height *= Global.ppt
+	_jump_velocity = sqrt(_jump_height * gravity * 2) * -1
 
 #region Public Methods
 
 func face_left():
-	return
+	_sprite.flip_h = true
 	
 func face_right():
-	pass
+	_sprite.flip_h = false
 	
 func run(direction : float):
 	_direction = direction
 
 func jump():
 	if is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = _jump_velocity 
+		
+func stop_jump():
+	if velocity.y < 0:
+		velocity.y = 0
+
 #endregion
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+func _physics_process(delta : float):
+	if sign(_direction) == -1:
+		face_left()
+	elif sign(_direction) == 1:
+		face_right()
 
-	# decelerate to zero
+	
+	if is_on_floor():
+		_ground_physics(delta)
+	else:
+		_air_physics(delta)
+	move_and_slide()
+	
+func _ground_physics(delta : float):
+		# decelerate to zero
 	if _direction == 0:
 		velocity.x = move_toward(velocity.x, 0, _deceleration * delta)
 	# accelerate from not moving, or trying to movbe in same direction
@@ -45,6 +67,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * delta)
 	# decelerate if trying to move in opposite direction
 	else:
-		velocity.x = move_toward(velocity.x, _direction * _speed, _deceleration * delta)
+		velocity.x = move_toward(velocity.x, _direction * _speed, _deceleration * delta)	
 
-	move_and_slide()
+func _air_physics(delta : float):
+		velocity.y += gravity * delta
+		if _direction:
+			velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _air_control * delta)
+
+
+	

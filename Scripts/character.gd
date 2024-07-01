940ada67f58071362f52dd8d5c06ab76405656bd
Amanda Smith <ajs.amandasmith@gmail.com>
2024-06-30 21:20:21 -0500

    Water

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+class_name Character extends CharacterBody2D
 
 @export_category("Locomotion")
 @export var _speed = 8
@@ -10,12 +10,19 @@ extends CharacterBody2D
 @export var _air_control : float = 0.5
 @export var _jump_dust : PackedScene
 var _jump_velocity : float
+var _was_on_floor : bool
 
 @export_category("Sprite")
 @export var _is_facing_left : bool
 @export var _sprites_face_left : bool
 @onready var _sprite : Sprite2D = $Sprite2D
-var _was_on_floor : bool
+
+@export_category("Swim")
+@export var _density : float = -0.1
+@export var _drag : float = 0.5
+var _water_surface_height : float
+var _is_in_water : bool
+var _is_below_surface : bool
 
 signal changed_direction(is_facing_left : bool)
 signal landed(floor_height : float)
@@ -49,13 +56,33 @@ func run(direction : float):
 	_direction = direction
 
 func jump():
+	if _is_in_water:
+		if _is_below_surface:
+				velocity.y = _jump_velocity * _drag
+				landed.emit(position.y)
+		else:
+				velocity.y = _jump_velocity
 	if is_on_floor():
 		velocity.y = _jump_velocity
 		_spawn_dust(_jump_dust)
 		
 func stop_jump():
-	if velocity.y < 0:
+	if velocity.y < 0 && not _is_in_water:
 		velocity.y = 0
+		
+func enter_water(water_surface_height : float):
+	_water_surface_height = water_surface_height
+	_is_in_water = true
+	_is_below_surface = false
+	landed.emit(position.y)
+	if velocity.y > 0:
+		velocity.y *= _drag
+	#
+func exit_water():
+	_is_in_water = false
+	#
+func dive():
+	_is_below_surface = true
 
 #endregion
 
@@ -64,14 +91,16 @@ func _physics_process(delta : float):
 		face_left()
 	elif _is_facing_left && sign(_direction) == 1:
 		face_right()
-	if is_on_floor():
+	if _is_in_water:
+		_water_physics(delta)
+	elif is_on_floor():
 		_ground_physics(delta)
 	else:
 		_air_physics(delta)
 	_was_on_floor = is_on_floor()
 	move_and_slide()
 	if not _was_on_floor && is_on_floor():
-		_landed()
+		_land()
 	
 func _ground_physics(delta : float):
 		# decelerate to zero
@@ -89,7 +118,19 @@ func _air_physics(delta : float):
 	if _direction:
 		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _air_control * delta)
 		
-func _landed():
+func _water_physics(delta : float):
+	if _direction == 0:
+		velocity.x = move_toward(velocity.x, 0, _deceleration * _drag * delta)
+	else:
+		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _drag * delta)
+	if _is_below_surface || _density > 0:
+		velocity.y = move_toward(velocity.y, gravity * _density * _drag, gravity * _drag * delta)
+	elif position.y - float(Global.ppt) / 4 < _water_surface_height:
+		velocity.y = move_toward(velocity.y, gravity * _density * _drag, gravity * _drag * delta)
+	else:
+		velocity.y = move_toward(velocity.y, gravity * _density * _drag * -1, gravity * _drag * delta)
+		
+func _land():
 	landed.emit(position.y)
 #
 func _spawn_dust(dust : PackedScene):
